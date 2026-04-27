3ec23403f6ecd001e7ccd3a74b328a8f4e546d8c
blairMe <felixanachoni@gmail.com>
2026-04-27 14:18:12 +0300

    add LDAP login endpoint to URL routing

--- healthcare_hub/users/urls.py
+++ healthcare_hub/users/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import LoginAPIView, UsersViewSet
+from .views import LoginAPIView, UsersViewSet, LDAPLoginAPIView
 
 router = DefaultRouter()
 router.register(r'users', UsersViewSet, basename='users')
@@ -8,5 +8,6 @@ router.register(r'users', UsersViewSet, basename='users')
 urlpatterns = [
     path('', include(router.urls)),
     path("login/", LoginAPIView.as_view(), name="login"),
+    path("login-ldap/", LDAPLoginAPIView.as_view(), name="login-ldap"),
 
 ]
