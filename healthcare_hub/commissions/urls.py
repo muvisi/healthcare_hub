30586b34bbd98530cc81ff698ada1dc122b167d4
blairMe <felixanachoni@gmail.com>
2026-05-04 20:06:32 +0300

    add route for detailed commission records view

--- healthcare_hub/commissions/urls.py
+++ healthcare_hub/commissions/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import CommissionRecordsView
+from .views import CommissionRecordsView, DetailedCommissionRecordsView
 
 urlpatterns = [
     path('records/', CommissionRecordsView.as_view(), name='commission-records'),
+    path('detailed-records/', DetailedCommissionRecordsView.as_view(), name='detailed-commission-records'),
 ]
